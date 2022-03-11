d6ac5b61a0670de2de433c1a115ce31f0cee6418
Mehmet Can Aydın <mehmet1can1aydin@gmail.com>
2022-03-11 12:25:30 +0300

    v1.0 güncellemesi
    İlk ve temeli atmak için yapılmış bir sürüm.
    Sadece formun arka plan rengi ayarlanabiliyor.
    Oluşturulma tarihi: 16.11.2019

---
+++ KYW Form v1.0/Version 1.0/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Version_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSelectedColor = new System.Windows.Forms.Label();
+            this.btnSelectColor = new System.Windows.Forms.Button();
+            this.btnCreateForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form Rengi : ";
+            // 
+            // lblSelectedColor
+            // 
+            this.lblSelectedColor.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblSelectedColor.Location = new System.Drawing.Point(185, 0);
+            this.lblSelectedColor.Name = "lblSelectedColor";
+            this.lblSelectedColor.Size = new System.Drawing.Size(615, 450);
+            this.lblSelectedColor.TabIndex = 1;
+            // 
+            // btnSelectColor
+            // 
+            this.btnSelectColor.Location = new System.Drawing.Point(32, 67);
+            this.btnSelectColor.Name = "btnSelectColor";
+            this.btnSelectColor.Size = new System.Drawing.Size(102, 61);
+            this.btnSelectColor.TabIndex = 2;
+            this.btnSelectColor.Text = "Renk Seç";
+            this.btnSelectColor.UseVisualStyleBackColor = true;
+            this.btnSelectColor.Click += new System.EventHandler(this.BtnSelectColor_Click);
+            // 
+            // btnCreateForm
+            // 
+            this.btnCreateForm.Location = new System.Drawing.Point(32, 143);
+            this.btnCreateForm.Name = "btnCreateForm";
+            this.btnCreateForm.Size = new System.Drawing.Size(102, 61);
+            this.btnCreateForm.TabIndex = 3;
+            this.btnCreateForm.Text = "Formu Oluştur";
+            this.btnCreateForm.UseVisualStyleBackColor = true;
+            this.btnCreateForm.Click += new System.EventHandler(this.BtnCreateForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreateForm);
+            this.Controls.Add(this.btnSelectColor);
+            this.Controls.Add(this.lblSelectedColor);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSelectedColor;
+        private System.Windows.Forms.Button btnSelectColor;
+        private System.Windows.Forms.Button btnCreateForm;
+    }
+}
+
---
+++ KYW Form v1.0/Version 1.0/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version_1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form lastForm;
+
+        private void BtnSelectColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                lblSelectedColor.BackColor = cd.Color;
+            }
+        }
+
+        private void BtnCreateForm_Click(object sender, EventArgs e)
+        {
+            lastForm = new Form();
+            lastForm.BackColor = lblSelectedColor.BackColor;
+            lastForm.WindowState = FormWindowState.Maximized;
+            lastForm.Show();
+        }
+    }
+}
